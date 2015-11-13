4d84beb5d387a7c44657259b4392d0a9bf89bdf7
piyangkul <piyangkul4@hotmail.com>
2015-11-13 19:20:28 +0700

    เปลี่ยน icon

--- interface_template/template_nav_side.php
+++ interface_template/template_nav_side.php
@@ -6,22 +6,22 @@
                 <img src="../assets/img/find_user.png" class="img-circle" class="user-image img-responsive"/>
             </li>
             <li>
-                <a <?php if($p=="history_order") echo 'class="active-menu"';?>  href="../interface_history_order/history_order.php"><span class="fa fa-dashboard fa-3x"></span> History Order</a>
+                <a <?php if($p=="history_order") echo 'class="active-menu"';?>  href="../interface_history_order/history_order.php"><span class="fa fa-folder-open-o fa-3x"></span> History Order</a>
             </li>
             <li>
-                <a <?php if($p=="product") echo 'class="active-menu"';?> href="../interface_product/product.php"><i class="fa fa-desktop fa-3x"></i> Product </a>
+                <a <?php if($p=="product") echo 'class="active-menu"';?> href="../interface_product/product.php"><i class="fa fa-cube fa-3x"></i> Product </a>
             </li>
             <li>
-                <a <?php if($p=="factory") echo 'class="active-menu"';?> href="../interface_factory/factory.php"><i class="fa fa-bar-chart-o fa-3x"></i> Factory</a>
+                <a <?php if($p=="factory") echo 'class="active-menu"';?> href="../interface_factory/factory.php"><i class="fa fa-building-o fa-3x"></i> Factory</a>
             </li>	
             <li>
-                <a <?php if($p=="shop") echo 'class="active-menu"';?> href="../interface_shop/shop.php"><i class="fa fa-table fa-3x"></i> Shop </a>
+                <a <?php if($p=="shop") echo 'class="active-menu"';?> href="../interface_shop/shop.php"><i class="fa fa-shopping-cart fa-3x"></i> Shop </a>
             </li>
             <li>
-                <a <?php if($p=="transport") echo 'class="active-menu"';?> href="../transport/transport.php"><i class="fa fa-qrcode fa-3x"></i> Transport </a>
+                <a <?php if($p=="transport") echo 'class="active-menu"';?> href="../transport/transport.php"><i class="fa fa-truck fa-3x"></i> Transport </a>
             </li>
             <li>
-                <a <?php if($p=="membership") echo 'class="active-menu"';?> href="../membership/membership.php"><i class="fa fa-qrcode fa-3x"></i> Membership </a>
+                <a <?php if($p=="membership") echo 'class="active-menu"';?> href="../membership/membership.php"><i class="fa fa-users fa-3x"></i> Membership </a>
             </li>
             <!--
             <li>
