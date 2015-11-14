73d5e043d9012fb3352c6edbba1043d584cc588d
piyangkul <piyangkul4@hotmail.com>
2015-11-14 11:40:40 +0700

    แก้ไขconnect

--- config/connect.php
+++ config/connect.php
@@ -9,7 +9,7 @@ $con;
 function dbconnect() {
     global $con;
     if (!$con) {
-        $con = new PDO(DSN, DBUSER, DBPASS);
+        $con = new PDO(DSN, DB_USER, DB_PASS);
         $con->query("SET NAMES UTF8");
     }
     return $con;
