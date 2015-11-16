d1995d414a7e18e9ae774f8dc44a5d776ffa76d1
remnant021 <ichiizu021@gmail.com>
2015-11-16 14:21:56 +0700

    ทำร้านค้า //ยังไม่เสร็จ

--- interface_shop/action/action_delShop.php
+++ interface_shop/action/action_delShop.php
@@ -1,8 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once dirname(__FILE__) . '/../function/func_shop.php';
 
+$idshop = $_GET['idshop'];
+
+$checkDel = delShop($idshop);
+if ($checkDel) {
+   header("location: ../membership.php?action=delCompleted");
+} else {
+   header("location: ../membership.php?action=delError");
+}
--- interface_shop/function/func_shop.php
+++ interface_shop/function/func_shop.php
@@ -25,6 +25,30 @@ function add_shop($name_shop, $idprovince, $tel_shop, $address_shop, $detail_sho
     }
 }
 
+function editShop($name_shop, $idprovince, $tel_shop, $address_shop, $detail_shop, $idshop) {
+    $conn = dbconnect();
+    $SQLCommand = "UPDATE shop SET  name_shop='$name_shop',idprovince = '$idprovince' , tel_shop='$tel_shop',address_shop='$address_shop',detail_shop='$detail_shop'
+                WHERE idshop = '$idshop'";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":name_shop" => $idshop,
+                ":name_shop" => $name_shop,
+                ":province" => $idprovince,
+                ":tel_shop" => $tel_shop,
+                ":address_shop" => $address_shop,
+                ":detail_shop" => $detail_shop
+            )
+    );
+
+    if ($SQLPrepare->rowCount() > 0) {
+        return $conn->lastInsertId();
+    } else {
+        return false;
+    }
+}
+
 function getShops() {
      $conn = dbconnect();
     $SQLCommand = "SELECT idshop,name_shop, tel_shop , address_shop ,detail_shop, name_province,name_region FROM shop INNER JOIN province ON shop.idprovince = province.idprovince INNER JOIN region ON region.idregion = province.idregion ";
