12eeec9b13303bcc42aa6984a72fddff03818a3c
piyangkul <piyangkul4@hotmail.com>
2016-03-16 00:37:05 +0700

    nav_side_docket

--- interface_template/template_nav_side.php
+++ interface_template/template_nav_side.php
@@ -27,7 +27,7 @@
                 <a <?php if ($p == "shipment") echo 'class="active-menu"'; ?> href="../shipment/shipment1.php"><i class="fa fa-road fa-3x"></i> Shipment </a>
             </li>
             <li>
-                <a <?php if ($p == "docket") echo 'class="active-menu"'; ?> href="#"><i class="fa fa-file-text-o fa-3x"></i> Cover Page </a>
+                <a <?php if ($p == "docket") echo 'class="active-menu"'; ?> href="../docket/docket.php"><i class="fa fa-file-text-o fa-3x"></i> Cover Page </a>
             </li>
             <li>
                 <a <?php if ($p == "history_pay_factory") echo 'class="active-menu"'; ?> href="../history_pay_factory/history_pay_factory.php"><i class="fa fa-folder-open-o fa-3x"></i> History Pay Factory </a>
