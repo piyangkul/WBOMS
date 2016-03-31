c1b2a168c2ce33122e0a0314c5d0c7a787009491
piyangkul <piyangkul4@hotmail.com>
2016-03-31 15:28:21 +0700

    แก้ nav side

--- interface_template/template_nav_side.php
+++ interface_template/template_nav_side.php
@@ -7,22 +7,7 @@
             </li>
             <li>
                 <a <?php if ($p == "history_order") echo 'class="active-menu"'; ?>  href="../interface_add_order/order.php"><span class="fa fa-folder-open-o fa-3x"></span> Order </a>
-            </li>
-            <li>
-                <a <?php if ($p == "product") echo 'class="active-menu"'; ?> href="../interface_product/product.php"><i class="fa fa-cube fa-3x"></i> Product </a>
-            </li>
-            <li>
-                <a <?php if ($p == "factory") echo 'class="active-menu"'; ?> href="../interface_factory/factory.php"><i class="fa fa-building-o fa-3x"></i> Factory</a>
-            </li>	
-            <li>
-                <a <?php if ($p == "shop") echo 'class="active-menu"'; ?> href="../interface_shop/shop.php"><i class="fa fa-shopping-cart fa-3x"></i> Shop </a>
-            </li>
-            <li>
-                <a <?php if ($p == "discount_shop") echo 'class="active-menu"'; ?> href="../discount_shop/discount_shop.php"><i class="fa fa-dollar fa-3x"></i> Discount Shop </a>
-            </li>
-            <li>
-                <a <?php if ($p == "transport") echo 'class="active-menu"'; ?> href="../transport/transport.php"><i class="fa fa-truck fa-3x"></i> Transportation </a>
-            </li>
+            </li>           
             <li>
                 <a <?php if ($p == "shipment") echo 'class="active-menu"'; ?> href="../shipment/shipment1.php"><i class="fa fa-road fa-3x"></i> Shipment </a>
             </li>
@@ -40,6 +25,21 @@
             </li>
             <li>
                 <a <?php if ($p == "stat") echo 'class="active-menu"'; ?> href="../stat/stat.php"><i class="fa fa-line-chart fa-3x"></i> Statistic Finance</a>
+            </li>
+            <li>
+                <a <?php if ($p == "product") echo 'class="active-menu"'; ?> href="../interface_product/product.php"><i class="fa fa-cube fa-3x"></i> Product </a>
+            </li>
+            <li>
+                <a <?php if ($p == "factory") echo 'class="active-menu"'; ?> href="../interface_factory/factory.php"><i class="fa fa-building-o fa-3x"></i> Factory</a>
+            </li>	
+            <li>
+                <a <?php if ($p == "shop") echo 'class="active-menu"'; ?> href="../interface_shop/shop.php"><i class="fa fa-shopping-cart fa-3x"></i> Shop </a>
+            </li>
+            <li>
+                <a <?php if ($p == "discount_shop") echo 'class="active-menu"'; ?> href="../discount_shop/discount_shop.php"><i class="fa fa-dollar fa-3x"></i> Discount Shop </a>
+            </li>
+            <li>
+                <a <?php if ($p == "transport") echo 'class="active-menu"'; ?> href="../transport/transport.php"><i class="fa fa-truck fa-3x"></i> Transportation </a>
             </li>
             <li>
                 <a <?php if ($p == "membership") echo 'class="active-menu"'; ?> href="../membership/membership.php"><i class="fa fa-users fa-3x"></i> Membership </a>
