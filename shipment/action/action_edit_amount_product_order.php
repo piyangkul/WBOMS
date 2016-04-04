3a0610462dbd2c2a3805076ac8ee5c1f6690aef8
remnant021 <ichiizu021@gmail.com>
2016-04-04 20:29:47 +0700

    Shipment Amount Product

--- shipment/action/action_edit_amount_product_order.php
+++ shipment/action/action_edit_amount_product_order.php
@@ -6,14 +6,16 @@ print_r($_POST);
 echo '</pre>';
 
 $idproduct_order = $_GET['idproduct_order'];
+echo $idproduct_order;
 $idshipment_period = $_GET['idshipment_period'];
 $idfactory = $_GET['idfactory'];
 $idamount_product_order = $_POST['amount_product_order'];
+$idunit = $_POST['name_unit'];
 
-    $price = $_GET['price'];
-    $status_shipment_factory = $_GET['status_shipment'];
+$price = $_GET['price'];
+$status_shipment_factory = $_GET['status_shipment'];
 
-$checkEdit_Amount_Product_order = editProduct_order($idproduct_order, $idamount_product_order);
+$checkEdit_Amount_Product_order = editProduct_order($idproduct_order, $idamount_product_order,$idunit);
 if ($checkEdit_Amount_Product_order) {
     header("location: ../add_shipment3.php?idshipment_period=" . $idshipment_period . "&idfactory=" . $idfactory . "&price=" . $price . "&status_shipment=" . $status_shipment_factory . "&action=editProduct_orderCompleted");
 } else {
--- shipment/popup_edit_amount_product_order.php
+++ shipment/popup_edit_amount_product_order.php
@@ -2,18 +2,21 @@
 require_once 'function/func_shipment.php';
 ?>
 <?php
-    $idproduct_order = $_GET['idproduct_order'];
-    $idshipment_period = $_GET['idshipment_period'];
-    $idfactory = $_GET['idfactory'];
-    $getProduct_order = getProduct_orderByID($idproduct_order);
-    $val_name_product = $getProduct_order['name_product'];
-    $val_amount_product_order = $getProduct_order['amount_product_order'];
+$idproduct_order = $_GET['idproduct_order'];
+$idshipment_period = $_GET['idshipment_period'];
+$idfactory = $_GET['idfactory'];
+$getProduct_order = getProduct_orderByID($idproduct_order);
+$val_name_product = $getProduct_order['name_product'];
+$val_amount_product_order = $getProduct_order['amount_product_order'];
+$val_name_unit = $getProduct_order['name_unit'];
+$val_idunit = $getProduct_order['idunit'];
+$val_idproduct = $getProduct_order['idproduct'];
 //    echo $idshipment_period;
 
-    $price = $_GET['price'];
-    $status_shipment_factory = $_GET['status_shipment'];
+$price = $_GET['price'];
+$status_shipment_factory = $_GET['status_shipment'];
 ?>
-﻿<form class="form" action="action/action_edit_amount_product_order.php?idproduct_order=<?php echo $idproduct_order; ?>&idshipment_period=<?php echo $idshipment_period; ?>&idfactory=<?php echo $idfactory; ?>&price=<?php echo $price;?>&status_shipment=<?php echo $status_shipment_factory; ?>" method="post">
+﻿<form class="form" action="action/action_edit_amount_product_order.php?idproduct_order=<?php echo $idproduct_order; ?>&idshipment_period=<?php echo $idshipment_period; ?>&idfactory=<?php echo $idfactory; ?>&price=<?php echo $price; ?>&status_shipment=<?php echo $status_shipment_factory; ?>" method="post">
     <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="myModalLabel">แก้ไขจำนวนสินค้า</h4>
@@ -37,25 +40,23 @@ require_once 'function/func_shipment.php';
                         <input type="text" class="form-control" id="amount_product_order" name="amount_product_order" value="<?php echo $val_amount_product_order; ?>">
                     </div>
                 </div>
-<!--                <div class="form-group col-xs-12">
+                <div class="form-group col-xs-12">
                     <label for="name_unit">หน่วยสินค้า</label><label class="text-danger">*</label>
                     <div class="form-group input-group">
                         <span class="input-group-addon"><i class="fa fa-circle-o-notch"  ></i></span>
-                        <select class="form-control" id="name_transport" name="name_transport" required >
-                            <option selected value="">กรุณาเลือกหน่วยสินค้า</option>
-                            //<?php
-//                            require_once '../product/function/func_product.php';
-//                            $val_idproduct = $_GET['idproduct'];
-//                            $getProductUnit = getProductUnit($val_idproduct);
-//                            foreach ($getProductUnit as $value) {
-//                                if ($value['name_big'] == NULL) {
-//                                    continue;
-//                                }
-//                                $valUnit = $value['name'];
-//                                
-//                                ?>
-                                <option value="//<?php echo $val_idunit; ?>"><?php echo $valUnit; ?></option>
-                            <?php  ?>
+                        <select class="form-control" id="name_unit" name="name_unit" required >
+                            <option selected value="<?= $val_idunit; ?>"><?= $val_name_unit; ?></option>
+                            <?php
+                            $getUnit = getUnit($val_idproduct,$val_idunit);
+                            foreach ($getUnit as $value) {
+                                $val_idunit = $value['idunit'];
+                                $val_name_unit = $value['name_unit'];
+                                ?>
+                                <option value="<?= $val_idunit; ?>"><?= $val_name_unit; ?></option><?php
+                            }
+                            ?>
+           
+                            <?php ?>
                         </select>
                     </div>
                 </div>-->
