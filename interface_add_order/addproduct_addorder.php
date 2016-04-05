e828786831d1e6b84654722d8cc40c76b7b4cefb
remnant021 <ichiizu021@gmail.com>
2016-04-05 15:10:11 +0700

    1255
    1

--- interface_add_order/addproduct_addorder.php
+++ interface_add_order/addproduct_addorder.php
@@ -246,8 +246,8 @@ require_once '/function/func_addorder.php';
         <!-- /. WRAPPER  -->
         <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
         <!-- JQUERY SCRIPTS -->
-        <script src="../assets/js/jquery-1.10.2.js"></script>
-        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+       <!-- <script src="../assets/js/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>-->
         <!-- BOOTSTRAP SCRIPTS -->
         <script src="../assets/js/bootstrap.min.js"></script>
         <!-- METISMENU SCRIPTS -->
