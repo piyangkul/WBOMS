bdcaf2cd1788cf9b6bff262948452b39a251b528
piyangkul <piyangkul4@hotmail.com>
2016-04-27 01:01:33 +0700

    discount_shop

--- discount_shop/action/action_discount_shop_show.php
+++ discount_shop/action/action_discount_shop_show.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__) . '/../function/func_discount_shop.php';
             <th><div align="center">ลำดับ</div></th>
 <th><div align="center">รหัสร้านค้า</div></th>
 <th><div align="center">ร้านค้า</div></th>
-<th><div align="center">ขายลด</div></th>
+<th><div align="center">ขายลด%</div></th>
 <th><div align="center">ราคาขาย</div></th>
 <th><div align="center">วันที่อัพเดทล่าสุด</div></th>
 </tr>
@@ -29,7 +29,7 @@ require_once dirname(__FILE__) . '/../function/func_discount_shop.php';
         if ($value['type_money'] == "PERCENT") {
             $cost = $val_price_unit - (($val_price_difference / 100.0) * $val_price_unit);
         } else {
-            $cost = $val_price_unit - $val_price_difference;
+            $cost = $val_price_unit + $val_price_difference;
         }
         $val_date_difference = $value['date_difference'];
 //        $change_date_difference = date("d-m-Y", strtotime($val_date_difference));
@@ -40,7 +40,7 @@ require_once dirname(__FILE__) . '/../function/func_discount_shop.php';
             <td><?php echo $i; ?></td>
             <td><?php echo $val_shop_code; ?></td>
             <td><?php echo $val_name_shop; ?></td>
-            <td><?php echo $val_price_difference; ?><?php echo ($val_type_money == "PERCENT") ? "%" : "฿"; ?></td>
+            <td><?php echo number_format($val_price_difference, 2); ?><?php echo ($val_type_money == "PERCENT") ? "%" : "฿"; ?></td>
             <td class="text-right"><?php echo number_format($cost, 2); ?></td>
             <td><?php echo date_format($date_difference, 'd-m-Y'); ?></td>
         </tr>
