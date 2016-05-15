1f3fbbda79ff75cc356c6d22ff7aee56e50a0e25
piyangkul <piyangkul4@hotmail.com>
2016-05-15 21:17:23 +0700

    stat

---
+++ stat/chart_old.php
@@ -0,0 +1,171 @@
+﻿<?php
+require_once 'function/func_stat.php';
+session_start();
+if (!isset($_SESSION['member']))
+    header('Location: ../index.php');
+
+$p = 'stat';
+if (isset($_GET['p']) && !empty($_GET['p'])) {
+    $p = $_GET['p'];
+}
+$year_start = $_GET['year_start'];
+$month_start = $_GET['month_start'];
+$year_end = $_GET['year_end'];
+$month_end = $_GET['month_end'];
+
+//if (isset($_GET['get_year_start'])) {
+//    $year_start = $_GET['get_year_start'];
+//    $month_start = $_GET['get_month_start'];
+//    $year_end = $_GET['get_year_end'];
+//    $month_end = $_GET['get_month_end'];
+//}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>THIP WAREE Project</title>
+        <!-- BOOTSTRAP STYLES-->
+        <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+        <!-- FONTAWESOME STYLES-->
+        <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- MORRIS CHART STYLES-->
+        <link href="../assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+        <link href="../assets/css/custom.css" rel="stylesheet" />
+        <script src="../assets/js/jquery-1.10.2.js"></script>
+
+        <script src="../high_chart_table/highchartTable-min.js" type="text/javascript"></script>
+        <script src="../high_chart_table/highchartTable.js" type="text/javascript"></script>
+
+        <!--        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" />
+                <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+                <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>-->
+
+    </head>
+    <body>
+        <div id="wrapper">
+            <!--  NAV TOP  -->
+            <?php include '../interface_template/template_nav_top.php'; ?>  
+
+            <!--  NAV SIDE  -->
+            <?php include '../interface_template/template_nav_side.php'; ?>  
+            <div id="page-wrapper" >
+                <div id="page-inner">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2> Statistic </h2>   
+                            <h5> สถิติการเงิน </h5>
+                        </div>
+                    </div>
+                    <!-- /. ROW  -->
+                    <hr/>
+                    <a class="btn btn-danger btn-lg" onclick="Back()"><span class="fa fa-arrow-circle-left"></span> Back</a>
+                    <br/>
+                    <!-- ข้อมูลการเงินรายเดือน-ปี -->
+                    <table class="highchart" data-graph-container-before="1" data-graph-type="column" style="display:none" data-graph-xaxis-end-on-tick="1" data-graph-height="550" data-graph-subtitle-text="ประกอบด้วย 1.รายรับ 2.รายจ่าย 3.กำไร/ขาดทุน แต่ละรอบ">
+                        <caption>กราฟแสดงข้อมูลการเงินรายเดือน-ปี</caption>
+                        <thead>
+                            <tr>                                  
+                                <th>รอบที่</th><!-- เดือน -->                                      
+                                <th>รายรับจากร้านค้า</th>
+                                <th>รายจ่ายของโรงงาน</th>
+                                <th>กำไร/ขาดทุน</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            //ดึงข้อมูลจากตาราง
+                            $getIncome_Outcome = getIncome_Outcome($month_start, $year_start, $month_end, $year_end);
+                            foreach ($getIncome_Outcome as $value) {
+                                $val_date_start = $value['date_start'];
+                                $date_start = date_create($val_date_start);
+                                $date_start->add(new DateInterval('P543Y0M0DT0H0M0S'));
+                                $val_date_end = $value['date_end'];
+                                $date_end = date_create($val_date_end);
+                                $date_end->add(new DateInterval('P543Y0M0DT0H0M0S'));
+                                $val_income = $value['income'];
+                                $val_outcome = $value['outcome'];
+                                $profit = $val_income - $val_outcome;
+                                ?>
+                                <tr>
+                                    <td><?php
+                            echo date_format($date_start, 'd-m-Y');
+                            echo" ถึง ";
+                            echo date_format($date_end, 'd-m-Y');
+                                ?>
+                                    </td>
+                                    <td><?php echo $val_income; ?></td>   
+                                    <td><?php echo $val_outcome; ?></td>   
+                                    <td><?php echo $profit; ?></td> 
+                                </tr>
+
+                                <?php
+                            }
+                            ?>
+
+                        </tbody>
+                    </table>
+                    <!-- End ข้อมูลการเงินรายเดือน-ปี -->
+
+                </div>
+                <!-- /. PAGE INNER  -->
+            </div>
+            <!-- /. PAGE WRAPPER  -->
+        </div>
+        <!-- /. WRAPPER  -->
+        <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+        <!-- JQUERY SCRIPTS -->
+<!--        <script src="../assets/js/jquery-1.10.2.js"></script>-->
+        <!-- BOOTSTRAP SCRIPTS -->
+        <script src="../assets/js/bootstrap.min.js"></script>
+        <!-- METISMENU SCRIPTS -->
+        <script src="../assets/js/jquery.metisMenu.js"></script>
+        <!-- DATA TABLE SCRIPTS -->
+        <script src="../assets/js/dataTables/jquery.dataTables.js"></script>
+        <script src="../assets/js/dataTables/dataTables.bootstrap.js"></script>
+
+        <script>
+                        $(document).ready(function () {
+                            $('#dataTables-example').dataTable();
+                        });
+        </script>
+        <script>
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip();
+                $('table.highchart').highchartTable();
+            });
+
+            $(document.body).on('hidden.bs.modal', function () {
+                $('#myModal').removeData('bs.modal');
+            });
+            function Back() {
+                window.location.assign("stat.php?year_start=<?php echo $year_start; ?>&month_start=<?php echo $month_start; ?>&year_end=<?php echo $year_end; ?>&month_end=<?php echo $month_end; ?>");
+            }
+        </script>
+
+    </body>
+</html>
+<div class="modal fade" id="myModal-lg" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content modal-lg">
+            <!-- Content -->
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="myModal-sm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content modal-sm">
+            <!-- Content -->
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Content -->
+        </div>
+    </div>
+</div>
+<!--<div class="alert alert-danger" role="alert">1.รง.ไทยฟูดส์เพี้ยน </div>-->
--- stat/function/func_stat.php
+++ stat/function/func_stat.php
@@ -29,17 +29,17 @@ function getIncome_Outcome($month_start, $year_start, $month_end, $year_end) {
         $month_end = '02';
     } elseif ($month_end == 3) {
         $month_end = '03';
-    }elseif ($month_end == 4) {
+    } elseif ($month_end == 4) {
         $month_end = '04';
-    }elseif ($month_end == 5) {
+    } elseif ($month_end == 5) {
         $month_end = '05';
-    }elseif ($month_end == 6) {
+    } elseif ($month_end == 6) {
         $month_end = '06';
-    }elseif ($month_end == 7) {
+    } elseif ($month_end == 7) {
         $month_end = '07';
-    }elseif ($month_end == 8) {
+    } elseif ($month_end == 8) {
         $month_end = '08';
-    }elseif ($month_end == 9) {
+    } elseif ($month_end == 9) {
         $month_end = '09';
     }
     $conn = dbconnect();
@@ -61,6 +61,12 @@ function getIncome_Outcome($month_start, $year_start, $month_end, $year_end) {
         array_push($resultArr, $result);
     }
     return $resultArr;
+}
+
+//chart
+function getIncome_Outcome_JSON($month_start, $year_start, $month_end, $year_end) {
+    $resultArr = getIncome_Outcome($month_start, $year_start, $month_end, $year_end);
+    return json_encode($resultArr, JSON_NUMERIC_CHECK); //, JSON_UNESCAPED_UNICODE);
 }
 
 //popup_stat_income
@@ -196,4 +202,194 @@ function getMonth_endThisYear() {
         array_push($resultArr, $result);
     }
     return $resultArr;
+}
+
+//chart
+function getsum_sale($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(sale) AS sum_sale FROM
+(SELECT ((order_shipment.price_unit-((order_shipment.difference_amount_product/100)*order_shipment.price_unit))*order_shipment.amount_product_order) AS sale,
+order_shipment.price_unit,order_shipment.difference_amount_product,order_shipment.amount_product_order FROM(SELECT order_transport.idorder_transport,order_transport.idtransport,order_transport.volume,order_transport.number,order_transport.price_transport,order_transport.status_shipment,product.name_product,product_order.amount_product_order,unit.name_unit,unit.price_unit,product.difference_amount_product,product_order.difference_product_order,product_order.type_product_order FROM transport JOIN order_transport ON transport.idtransport=order_transport.idtransport JOIN shipment_period ON order_transport.shipment_period_idshipment_period=shipment_period.idshipment_period JOIN product_order ON order_transport.product_order_idproduct_order=product_order.idproduct_order JOIN unit ON product_order.idunit=unit.idunit JOIN product ON unit.idproduct=product.idproduct JOIN factory ON product.idfactory=factory.idfactory WHERE shipment_period.idshipment_period=:idshipment_period ) AS order_shipment ) AS sale ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_sale'];
+}
+
+//chart
+function getPrice_transport($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(price_transport) AS sum_price_transport
+FROM(SELECT shipment_period.idshipment_period,factory.idfactory,factory.name_factory,product.name_product,product_order.amount_product_order,order_transport.price_transport 
+ FROM shipment_period JOIN order_transport ON shipment_period.idshipment_period = order_transport.shipment_period_idshipment_period 
+ JOIN product_order ON order_transport.product_order_idproduct_order = product_order.idproduct_order 
+ JOIN unit ON product_order.idunit = unit.idunit 
+ JOIN product ON product.idproduct = unit.idproduct JOIN factory ON factory.idfactory=product.idfactory 
+ WHERE shipment_period.idshipment_period = :idshipment_period  GROUP BY order_transport.idtransport,order_transport.date_transport,order_transport.volume,order_transport.number) AS tab ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_price_transport'];
+}
+
+//chart
+function getsum_refund($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(refund)AS sum_refund FROM 
+(SELECT ((refund_shipment.price_unit-((refund_shipment.difference_amount_product/100)*refund_shipment.price_unit))*refund_shipment.amount_product_refunds) AS refund
+FROM(SELECT shipment_period.idshipment_period, shop.idshop,shop.name_shop,factory.idfactory,factory.name_factory,product.idproduct,product.name_product,unit.price_unit,unit.name_unit,product_refunds.amount_product_refunds,product.difference_amount_product,factory.difference_amount_factory,difference.price_difference,difference.type_money,product_refunds.price_product_refunds FROM order_product_refunds JOIN product_refunds ON order_product_refunds.idorder_product_refunds=product_refunds.order_product_refunds_idorder_product_refunds JOIN unit ON product_refunds.idunit=unit.idunit JOIN product ON product.idproduct=unit.idproduct JOIN factory ON product.idfactory=factory.idfactory JOIN difference ON product.idproduct=difference.idproduct JOIN shop ON order_product_refunds.shop_idshop=shop.idshop JOIN shipment_period ON order_product_refunds.shipment_period_idshipment_period=shipment_period.idshipment_period WHERE product_refunds.idunit=unit.idunit AND difference.idshop=shop.idshop  AND shipment_period.idshipment_period=:idshipment_period) AS refund_shipment)AS refund ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_refund'];
+}
+
+//chart
+function getIncome_Outcome_total($getIncome_Outcome) {
+    $arrAll = array();
+    foreach ($getIncome_Outcome as $value) {
+        $_idshipment_period = $value['idshipment_period'];
+
+        $getsum_refund = getsum_refund($_idshipment_period);
+        $getPrice_transport = getPrice_transport($_idshipment_period);
+        $getsum_sale = getsum_sale($_idshipment_period);
+
+        $getsum_sale_income = getsum_sale_income($_idshipment_period);
+        $getdebt = getdebt($_idshipment_period);
+        $getsum_sale_income_Last = getsum_sale_income_Last($_idshipment_period);
+        $getPrice_transport_Last = getPrice_transport_Last($_idshipment_period);
+        $getsum_refund_income = getsum_refund_income($_idshipment_period);
+
+        $all_outcome = $getsum_sale + $getPrice_transport - $getsum_refund;
+        $all_income = $getsum_sale_income + $getPrice_transport + $getdebt + $getsum_sale_income_Last + $getPrice_transport_Last - $getsum_refund_income; //$getsum_sale_income + $getPrice_transport + $getdebt + $getsum_sale_income_Last + $getPrice_transport_Last - $getsum_refund_income
+
+        $arrRes = array(
+            "idshipment_period" => $_idshipment_period,
+            "date_start" => $value['date_start'],
+            "date_end" => $value['date_end'],
+            "income" => $value['income'],
+            "outcome" => $value['outcome'],
+            "all_outcome" => $all_outcome,
+            "all_income" => $all_income
+        );
+        array_push($arrAll, $arrRes);
+    }
+    return $arrAll;
+}
+
+//chart
+function getIncome_Outcome_total_JSON($getIncome_Outcome) {
+    $arrAll = getIncome_Outcome_total($getIncome_Outcome);
+    return json_encode($arrAll, JSON_NUMERIC_CHECK); //, JSON_UNESCAPED_UNICODE);
+}
+
+//chart
+function getsum_sale_income($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(sale) AS sum_sale FROM
+(SELECT IF(product_docket.type_product_order='PERCENT',((product_docket.price_unit-((product_docket.difference_product_order/100)*product_docket.price_unit))*product_docket.amount_product_order),(product_docket.price_unit+product_docket.difference_product_order)* product_docket.amount_product_order) AS sale,product_docket.idshop,
+product_docket.price_unit,product_docket.difference_product_order,product_docket.amount_product_order,product_docket.type_product_order FROM
+(SELECT view_product_order_shipment.idshop,view_product_order_shipment.name_product,view_product_order_shipment.amount_product_order,view_product_order_shipment.name_unit,view_product_order_shipment.price_unit,view_product_order_shipment.difference_product_order,view_product_order_shipment.type_product_order FROM view_product_order_shipment LEFT JOIN view_transport_shipment ON view_product_order_shipment.idproduct_order = view_transport_shipment.product_order_idproduct_order WHERE view_transport_shipment.idshipment_period=:idshipment_period )AS product_docket)AS sale ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_sale'];
+}
+
+//chart
+function getdebt($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(debt)AS debt FROM `pay` JOIN shipment_period ON pay.idshipment_period=shipment_period.idshipment_period WHERE shipment_period.idshipment_period=:idshipment_period-1 ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['debt'];
+}
+
+//chart
+function getsum_sale_income_Last($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(sale) AS sum_sale FROM
+(SELECT AA.sale,AA.price_unit,AA.difference_product_order,AA.amount_product_order,AA.type_product_order FROM
+(SELECT IF(product_docket.type_product_order='PERCENT', ((product_docket.price_unit-((product_docket.difference_product_order/100)*product_docket.price_unit))*product_docket.amount_product_order),(product_docket.price_unit+product_docket.difference_product_order)* product_docket.amount_product_order) AS sale,product_docket.idshop, product_docket.price_unit,product_docket.difference_product_order,product_docket.amount_product_order,product_docket.type_product_order FROM 
+ (SELECT view_product_order_shipment.idshop,view_product_order_shipment.name_product,view_product_order_shipment.amount_product_order,view_product_order_shipment.name_unit,view_product_order_shipment.price_unit,view_product_order_shipment.difference_product_order,view_product_order_shipment.type_product_order 
+ FROM view_product_order_shipment LEFT JOIN view_transport_shipment ON view_product_order_shipment.idproduct_order = view_transport_shipment.product_order_idproduct_order WHERE view_transport_shipment.idshipment_period=:idshipment_period-1)AS product_docket)AS AA
+ JOIN 
+(SELECT A.idshop,A.name_shop,province.idprovince,province.name_province,region.idregion,region.name_region,A.idshipment_period,A.date_start,A.date_end,pay.date_pay,pay.debt,pay.price_pay,pay.status_pay,pay.status_process FROM (SELECT shop.idshop,shop.name_shop,shop.idprovince,shipment_period.idshipment_period,shipment_period.date_start,shipment_period.date_end FROM shop,shipment_period) AS A left JOIN pay on A.idshop=pay.shop_idshop and A.idshipment_period=pay.idshipment_period JOIN province ON province.idprovince=A.idprovince JOIN region ON region.idregion=province.idregion where A.idshipment_period=:idshipment_period-1 AND pay.date_pay IS NULL )AS BB
+ON AA.idshop=BB.idshop)AS sale ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_sale'];
+}
+
+//chart
+function getPrice_transport_Last($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT SUM(all_transport.price_transport) AS sum_price_transport FROM
+(SELECT AA.price_transport FROM(SELECT all_transport.idshipment_period,all_transport.idshop,all_transport.idfactory,all_transport.name_factory,all_transport.name_product,all_transport.amount_product_order,all_transport.price_transport
+FROM(SELECT shipment_period.idshipment_period,shop.idshop,factory.idfactory,factory.name_factory,product.name_product,product_order.amount_product_order,order_transport.price_transport FROM shipment_period JOIN order_transport ON shipment_period.idshipment_period = order_transport.shipment_period_idshipment_period 
+ JOIN product_order ON order_transport.product_order_idproduct_order = product_order.idproduct_order 
+JOIN order_p ON product_order.idorder_p= order_p.idorder_p 
+JOIN shop ON order_p.idshop=shop.idshop
+ JOIN unit ON product_order.idunit = unit.idunit 
+ JOIN product ON product.idproduct = unit.idproduct JOIN factory ON factory.idfactory=product.idfactory 
+ WHERE shipment_period.idshipment_period =:idshipment_period-1  GROUP BY order_transport.idtransport,order_transport.date_transport,order_transport.volume,order_transport.number) AS all_transport )AS AA
+JOIN 
+(SELECT A.idshop,A.name_shop,province.idprovince,province.name_province,region.idregion,region.name_region,A.idshipment_period,A.date_start,A.date_end,pay.date_pay,pay.debt,pay.price_pay,pay.status_pay,pay.status_process FROM (SELECT shop.idshop,shop.name_shop,shop.idprovince,shipment_period.idshipment_period,shipment_period.date_start,shipment_period.date_end FROM shop,shipment_period) AS A left JOIN pay on A.idshop=pay.shop_idshop and A.idshipment_period=pay.idshipment_period JOIN province ON province.idprovince=A.idprovince JOIN region ON region.idregion=province.idregion where A.idshipment_period=:idshipment_period-1 AND pay.date_pay IS NULL )AS BB
+ON AA.idshop=BB.idshop)AS all_transport ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_price_transport'];
+}
+
+//chart
+function getsum_refund_income($idshipment_period) {
+    $conn = dbconnect();
+    $SQLCommand = "SELECT sum(order_product_refunds.order_price_product_refunds)AS sum_refund FROM `order_product_refunds` WHERE `shipment_period_idshipment_period`=:idshipment_period ";
+
+    $SQLPrepare = $conn->prepare($SQLCommand);
+    $SQLPrepare->execute(
+            array(
+                ":idshipment_period" => $idshipment_period
+            )
+    );
+    $result = $SQLPrepare->fetch(PDO::FETCH_ASSOC);
+    return $result['sum_refund'];
 }
