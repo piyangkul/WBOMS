dfd3ba761dad25c4234f6775704cd56951483dde
remnant021 <ichiizu021@gmail.com>
2016-05-18 13:05:20 +0700

    Delete_File

--- model/db_connect.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DSN","mysql:dbname=project;host=localhost");
-define("DB_USER", "root");
-define("DB_PASS", "1111");
-$con = new PDO(DSN, DB_USER, DB_PASS);
-$con->query("SET NAMES UTF8");
-?>
